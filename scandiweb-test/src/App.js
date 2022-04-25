92909a68aa32f79e663afaf36e200211bcc95405
jo-rdan <jordankayinamura@gmail.com>
2022-04-26 00:42:42 +0200

    - [x] App setup - [x] setup project/folder structure

---
+++ scandiweb-test/src/App.js
@@ -0,0 +1,12 @@
+import {Component} from "react";
+
+class App extends Component{
+  render() {
+    return (
+
+      <p>This is my first class-based Component, well not really tho</p>
+      )
+  }
+}
+
+export default App;
